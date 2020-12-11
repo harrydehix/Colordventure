723e9cf2b5c2ea45f751252067f3559735205095
harrydehix <harry.holly02@gmail.com>
2020-12-11 23:31:51 +0100

    Add application icon and runnable files

--- colordventureMavenProject/src/main/java/com/harrydehix/colordventure/App.java
+++ colordventureMavenProject/src/main/java/com/harrydehix/colordventure/App.java
@@ -6,6 +6,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -16,6 +17,7 @@ public class App extends Application {
     	App.stage = stage;
     	
         stage.setScene(new Scene(loadFXML("main", new Controller()), 640, 480));
+        stage.getIcons().add(new Image(App.class.getResource("/icons/icon.png").toString()));
         stage.setTitle("Colordventure - v1.0.0");
         stage.show();
     }
---
+++ colordventureMavenProject/src/main/java/com/harrydehix/colordventure/SuperMain.java
@@ -0,0 +1,7 @@
+package com.harrydehix.colordventure;
+
+public class SuperMain {
+	public static void main(String[] args) {
+		App.main(args);
+	}
+}
